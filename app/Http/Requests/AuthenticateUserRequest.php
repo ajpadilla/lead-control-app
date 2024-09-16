f40a82c6fc4efc42532c1089bcca1c8d95563850
Alvaro Padilla <ajpadilla88@gmail.com>
2024-09-16 18:49:54 -0500

    Agregando FormRequest para validar autenticacion de usuarios

---
+++ app/Http/Requests/AuthenticateUserRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthenticateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'username.required' => 'The username field is required.',
+            'password.required' => 'The password field is required.',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     * @throws ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $response = response()->json([
+            'meta' => [
+                'success' => false,
+                'errors' => $validator->errors()->all(),
+            ],
+        ], 422);
+
+        throw new ValidationException($validator, $response);
+    }
+}
