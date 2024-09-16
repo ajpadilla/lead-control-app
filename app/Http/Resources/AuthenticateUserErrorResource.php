2e6c9c815587e9bcf5d266f286fc2462f6ba9f9a
Alvaro Padilla <ajpadilla88@gmail.com>
2024-09-16 18:51:04 -0500

    Agregando resource para retornar errores al autenticar usuarios

---
+++ app/Http/Resources/AuthenticateUserErrorResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthenticateUserErrorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'meta' => [
+                'success' => false,
+                'errors' => $this->resource,
+            ],
+        ];
+
+    }
+}
