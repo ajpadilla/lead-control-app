12dcd460a2200ed68e45dda0004b8c0b92252d74
Alvaro Padilla <ajpadilla88@gmail.com>
2024-09-16 19:00:53 -0500

    Agregando implementacion concreta para cada repositorio

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\Lead\EloquentLeadRepository;
+use App\Repositories\Lead\LeadRepository;
+use App\Repositories\Permission\EloquentPermissionRepository;
+use App\Repositories\Permission\PermissionRepository;
+use App\Repositories\Role\EloquentRoleRepository;
+use App\Repositories\Role\RoleRepository;
+use App\Repositories\User\EloquentUserRepository;
+use App\Repositories\User\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(RoleRepository::class, EloquentRoleRepository::class);
+        $this->app->bind(PermissionRepository::class, EloquentPermissionRepository::class);
+        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+        $this->app->bind(LeadRepository::class, EloquentLeadRepository::class);
     }
 
     /**
